ae75eed6d2bc50cb821ae4988d0bca1970c2175e
R Sreehari <rsreehari091@gmail.com>
2025-12-10 19:20:46 +0530

    created a component in it created a Loading Spinner

---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const LoadingSpinner: React.FC = () => (
+  <div className="flex items-center justify-center h-screen bg-gray-50">
+    <div className="text-center">
+      <div className="animate-spin rounded-full h-16 w-16 border-b-2 border-blue-600 mx-auto mb-4"></div>
+      <p className="text-gray-600 text-lg">Loading books from OPDS catalog...</p>
+    </div>
+  </div>
+);
